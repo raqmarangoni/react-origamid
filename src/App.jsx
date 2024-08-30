a13ce4863a7d5239ea146881eb2cbbe24dd267b4
Raquel Marangoni <rmarangonisan@gmail.com>
2024-08-29 23:42:46 -0300

    feat: exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React from "react";
+
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
 
 const App = () => {
-  return <div>App React</div>;
-};
+  const dados = luana;
+  const total = dados.compras.map((item) => Number(item.preco.replace('R$', ''))).reduce((a, b) => a + b);
+  
+
+
+
+  return (<div>
+    <p>Nome: {dados.cliente}</p>
+    <p>Idade: {dados.idade}</p>
+    <p>Situação: {dados.ativa ? 'ativa' : 'inativa'}</p>
+    <p>Total gasto: R$ {total}</p>
+    <p>
+      Situação: {' '}
+      <span style={{ color: dados.ativa ? 'green' : 'red' }}>
+        {dados.ativa ? 'Ativa' : 'Inativa'}
+      </span>
+      <p>TOtal: R$ {total}</p>
+      <p>{total > 10000 && 'Você está gastando muito'}</p>
+    </p>
+
+  </div>
+)};
+
+
+
+
 
 export default App;
